bf893a85c190db41d9a8f0663a53bad3beef34bf
robertsoroka <soroka.robert8@gmail.com>
2025-01-25 22:04:57 +0100

    web dev  - pridanie zamestnanca 2

---
+++ admin_automator/admin_automator/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for admin_automator project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('app_one/', include('zentak_legal_automator.urls')),  # Include app_one URLs
+    #path('app_two/', include('zentak_leads_automator.urls')),  # Include app_two URLs
+    path('app_three/', include('zentak_fintech_automator.urls')),
+    path('app_four/', include('zentak_payroller_automator.urls')),
+]
