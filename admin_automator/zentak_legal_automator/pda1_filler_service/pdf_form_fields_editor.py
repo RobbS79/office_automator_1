e2b7d2cf24ecb7456f7d5d9fed7224ed5061c2ff
robertsoroka <soroka.robert8@gmail.com>
2025-02-05 22:57:46 +0100

    web dev  - feature generovanie pda1 11

--- admin_automator/zentak_legal_automator/pda1_filler_service/pdf_form_fields_editor.py
+++ admin_automator/zentak_legal_automator/pda1_filler_service/pdf_form_fields_editor.py
@@ -10,30 +10,30 @@ class PDFFormFiller:
             "~/office_automator_1/office_automator_1/pdf_forms/filled_form.pdf")
 
 
-def process_pda1_filler(self):
-        json_data = self.db_employee_json_object
-        data_form_fields = fillpdfs.get_form_fields(self.pda1_form_input_path)
-        data_obj = {"nazov_firmy": "ZENTAK s.r.o.",
-                    "ico_firmy": "52328121",
-                    "sidlo_firmy_ulica": "Postova 5",
-                    "sidlo_firmy_mesto": "sidlo_firmy_psc",
-                    "sidlo_firmy_stat": "Slovensko",
-                    "kontaktna_osoba_firmy": "Ing. Jozef Soroka",
+    def process_pda1_filler(self):
+            json_data = self.db_employee_json_object
+            data_form_fields = fillpdfs.get_form_fields(self.pda1_form_input_path)
+            data_obj = {"nazov_firmy": "ZENTAK s.r.o.",
+                        "ico_firmy": "52328121",
+                        "sidlo_firmy_ulica": "Postova 5",
+                        "sidlo_firmy_mesto": "sidlo_firmy_psc",
+                        "sidlo_firmy_stat": "Slovensko",
+                        "kontaktna_osoba_firmy": "Ing. Jozef Soroka",
 
-                    }
+                        }
 
-        for json_data_key, json_data_value in json_data.items():
-            print(json_data_key)
-            if json_data_key in data_form_fields.keys():
-                # Replace False boolean values with None
-                if json_data_value is False:
-                    data_obj[json_data_key] = None
-                else:
-                    data_obj[json_data_key] = json_data_value
+            for json_data_key, json_data_value in json_data.items():
+                print(json_data_key)
+                if json_data_key in data_form_fields.keys():
+                    # Replace False boolean values with None
+                    if json_data_value is False:
+                        data_obj[json_data_key] = None
+                    else:
+                        data_obj[json_data_key] = json_data_value
 
-        try:
-            fillpdfs.write_fillable_pdf(self.pda1_form_input_path, self.pda1_form_output_path, data_obj)
-        except KeyError as e:
-            print(f"Error generating PDF: {e}")
-            # Additional error handling can go here
+            try:
+                fillpdfs.write_fillable_pdf(self.pda1_form_input_path, self.pda1_form_output_path, data_obj)
+            except KeyError as e:
+                print(f"Error generating PDF: {e}")
+                # Additional error handling can go here
 
